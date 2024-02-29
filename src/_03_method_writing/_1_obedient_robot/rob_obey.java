d88cd832aba67823102b27391ae139f6879a4f9d
jereycui2008 <jeremycuid@gmail.com>
2024-02-28 19:59:31 -0800

    robert obey

---
+++ src/_03_method_writing/_1_obedient_robot/rob_obey.java
@@ -0,0 +1,10 @@
+package _03_method_writing._1_obedient_robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class rob_obey {
+public static void main(String[] args) {
+	Robot rob = new Robot();
+	rob.penDown();
+}
+}
