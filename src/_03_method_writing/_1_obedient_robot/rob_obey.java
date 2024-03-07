8ed007278751c22263a7554c8b5705abaf938544
jereycui2008 <jeremycuid@gmail.com>
2024-03-06 19:58:37 -0800

    OBEY

--- src/_03_method_writing/_1_obedient_robot/rob_obey.java
+++ src/_03_method_writing/_1_obedient_robot/rob_obey.java
@@ -3,8 +3,28 @@ package _03_method_writing._1_obedient_robot;
 import org.jointheleague.graphical.robot.Robot;
 
 public class rob_obey {
-public static void main(String[] args) {
-	Robot rob = new Robot();
-	rob.penDown();
-}
+	public static void main(String[] args) {
+		Robot rob = new Robot();
+		rob.setSpeed(100);
+		rob.penDown();
+		triangle(rob);
+	}
+	public static void square(Robot rob) {
+		for (int i = 0; i < 4; i++) {
+			rob.move(100);
+			rob.turn(90);
+		}
+		rob.penUp();
+		rob.move(150);
+	}
+	public static void triangle(Robot rob) {
+		rob.turn(45);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(135);
+		rob.move(13712	);
+		rob.penUp();
+		rob.move(150);
+	}
 }
