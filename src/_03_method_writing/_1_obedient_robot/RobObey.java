2cb2f11d4064be8df88db380fc147f5b2acf22d7
lvl0_module2 <zoey.lestyk@gmail.com>
2024-03-20 19:58:23 -0700

    I made a robot that draws shapes and a calculator.

--- src/_03_method_writing/_1_obedient_robot/RobObey.java
+++ src/_03_method_writing/_1_obedient_robot/RobObey.java
@@ -1,13 +1,24 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
-public class rob_obey {
+public class RobObey {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
 		rob.setSpeed(100);
 		rob.penDown();
-		triangle(rob);
+		String ask = JOptionPane.showInputDialog("Which shape do you want the robot to draw?\n(square, triangle, circle)");
+		if (ask.equals("square")) {
+			square(rob);
+		}
+		if (ask.equals("triangle")) {
+			triangle(rob);
+		}
+		if (ask.equals("circle")) {
+			circle(rob);	
+		}
 	}
 	public static void square(Robot rob) {
 		for (int i = 0; i < 4; i++) {
@@ -23,8 +34,16 @@ public class rob_obey {
 		rob.turn(90);
 		rob.move(100);
 		rob.turn(135);
-		rob.move(13712	);
+		rob.move(139);
 		rob.penUp();
 		rob.move(150);
+	}
+	public static void circle(Robot rob) {
+		for (int i =0; i <36; i++) {
+			rob.move(10);
+			rob.turn(10);
+		}
+		rob.penUp();
+		rob.move(150);	
 	}
 }
--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -8,24 +8,47 @@ public class Calculator {
 	public static void main(String[] args) {
 		String input1 = JOptionPane.showInputDialog("Enter a number");
 		String input2 = JOptionPane.showInputDialog("Enter another number");
-		int number1 = Integer.parseInt(input1);
-		int number2 = Integer.parseInt(input2);
+		double number1 = Double.parseDouble(input1);
+		double number2 = Double.parseDouble(input2);
 		int task = JOptionPane.showOptionDialog(null, "What do you choose?", "Calculator", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
-
 		// 3) Call the correct method depending on what option the user chooses
-		
+if (task == 0) {
+	JOptionPane.showMessageDialog(null, result(divide(number1,number2)));
+}
+if (task == 1) {
+	JOptionPane.showMessageDialog(null, result(multiply(number1,number2)));
+}
+if (task == 2) {
+	JOptionPane.showMessageDialog(null, result(subtract(number1,number2)));
+}
+if (task == 3) {
+	JOptionPane.showMessageDialog(null, result(add(number1,number2)));
+}
+
 		// 4) Call the result() method and put the answer in a pop-up
 	}
-	
+
 	// 1) Make 4 static methods (add, subtract, multiply, divide)
 	//		They should each take 2 numbers as parameters
 	//		and return the answer.
-	 
-	
+	public static double add(double number1, double number2) {
+		return number1 + number2;
+	}
+	public static double subtract(double number1, double number2) {
+		return number1 - number2;
+	}
+	public static double multiply(double number1, double number2) {
+		return number1 * number2;
+	}
+	public static double divide(double number1, double number2) {
+		return number1 / number2;
+	}
 	// 2) Make a static method called " result" that takes a number
 	//		as a parameter and returns the number in a string
 	//		EX: "Your answer is " + number;
+public static String result(double number) {
+	return "Your answer is " + number;
+}
 
-	
 }
